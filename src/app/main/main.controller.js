bd2a259560a8b3d2b270748721d264f49af982aa
Ravi <raviyerramilli@hotmail.com>
2016-09-17 12:03:55 +0530

    post form and button click

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,32 +1,12 @@
 export class MainController {
-  constructor ($timeout, webDevTec, toastr) {
+  constructor ($http) {
     'ngInject';
 
-    this.awesomeThings = [];
-    this.classAnimation = '';
-    this.creationDate = 1474089011120;
-    this.toastr = toastr;
 
-    this.activate($timeout, webDevTec);
   }
 
-  activate($timeout, webDevTec) {
-    this.getWebDevTec(webDevTec);
-    $timeout(() => {
-      this.classAnimation = 'rubberBand';
-    }, 4000);
-  }
+    postMessage() {
+        console.log("post");
+    }
 
-  getWebDevTec(webDevTec) {
-    this.awesomeThings = webDevTec.getTec();
-
-    angular.forEach(this.awesomeThings, (awesomeThing) => {
-      awesomeThing.rank = Math.random();
-    });
-  }
-
-  showToastr() {
-    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    this.classAnimation = '';
-  }
 }
