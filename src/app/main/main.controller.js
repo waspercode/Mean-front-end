bd7046ea99108c3171358c59d4a0f9337d658154
Ravi <raviyerramilli@hotmail.com>
2016-09-29 15:20:15 +0530

    Post from Angular to Node without Mongo

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,11 +2,12 @@ export class MainController {
   constructor ($http) {
     'ngInject';
 
+      this.$http = $http;
 
   }
 
     postMessage() {
-        console.log("post");
+        this.$http.post('http://localhost:5000/api/message', {msg: this.message});
     }
 
 }
