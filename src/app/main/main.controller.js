646edec29610351dd28219dc12319c2de90d4ea2
Ravi <raviyerramilli@hotmail.com>
2016-09-30 21:02:02 +0530

    create a message list view in angular

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,8 +3,15 @@ export class MainController {
     'ngInject';
 
       this.$http = $http;
+      this.getMessages();
 
   }
+    getMessages() {
+      var vm = this;
+        this.$http.post('http://localhost:5000/api/message').then(function(result){
+            vm.message = result.data;
+        });
+    }
 
     postMessage() {
         this.$http.post('http://localhost:5000/api/message', {msg: this.message});
